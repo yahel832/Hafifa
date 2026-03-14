be6b16f0d0a27d456126fdd2a0fca4824857ef94
yahel832 <queenyukipuki@gmail.com>
2026-03-14 22:14:02 +0200

    fixed- not passing the big array

--- frontend/javascript/arrow functions/optimizedFibonacci.js
+++ frontend/javascript/arrow functions/optimizedFibonacci.js
@@ -1,22 +1,22 @@
-const fibonacciByIndex = (index, sequence) => {
-    if (index >= 2) {
-        sequence[index] = sequence[index - 1] + sequence[index - 2];
+const fibonacciByIndex = (index, first, second) => {
+    if (index == 0) {
+        return [second, first];
     }
     
-    return sequence;
+    return [second, first + second];
 }
 
 const runFibonacci = (num = 10) => {
-    setTimeout(recursiveTimeout, 100, 0, num - 1, [0, 1]);
+    setTimeout(recursiveTimeout, 100, 0, num - 1, 0, 1);
 }
 
-const recursiveTimeout = (currIndex, limit, sequence) => {
-    newSequence = fibonacciByIndex(currIndex, sequence);
-    console.log(newSequence[currIndex])
+const recursiveTimeout = (currIndex, limit, first, second) => {
+    newSequence = fibonacciByIndex(currIndex, first, second);
+    console.log(newSequence[1])
     currIndex++;
 
     if (currIndex <= limit) {
-        setTimeout(recursiveTimeout, 100, currIndex, limit, newSequence);
+        setTimeout(recursiveTimeout, 100, currIndex, limit, newSequence[0], newSequence[1]);
     } else {
         console.log(`Execution finished after ${limit} calls.`);
     }
