78cffed7b2161237940ab4bb88fcfff7a8d8a23e
yahel832 <queenyukipuki@gmail.com>
2026-03-28 23:13:54 +0300

    Delete ToDoList directory

--- ToDoList/DataBase/Models/User.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from DataBase.Connector import Base
-
-
-class User(Base):
-    __tablename__ = 'user'
-    user_id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    password = Column(String)
--- ToDoList/Schemas/Todo.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class TodoBase(BaseModel):
-    todo_name: str
-    description: str
-    user_id: int
-    completed: bool
